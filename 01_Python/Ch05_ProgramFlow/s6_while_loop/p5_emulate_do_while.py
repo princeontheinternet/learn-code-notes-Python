e2a63052c966d24555dca2036ff50d9beccf046b
Prince Rathore <princeontheinternet@gmail.com>
2022-12-16 14:07:39 +0530

    updated

---
+++ 01_Python/Ch05_ProgramFlow/s6_while_loop/p5_emulate_do_while.py
@@ -0,0 +1,25 @@
+# A Do while loop is a loop that executes the body of the loop atleast once before checking the condition.
+
+# A do while state can be achieved in Python with the help of below code.
+
+n=0
+while True:
+    n += 1
+    print(n)
+    if n >= 5:
+        break
+
+print("*" * 80)
+
+while True:
+    number = int(input("Enter a negative number to break throuh the program: "))
+    print(number)
+    if not number >= 0:
+         break
+
+#   Output:
+# 1
+# 2
+# 3
+# 4
+# 5
