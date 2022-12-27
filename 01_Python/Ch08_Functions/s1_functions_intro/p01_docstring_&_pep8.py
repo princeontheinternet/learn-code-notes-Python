b57d27f4bd713e37b3d6342e79ba9406c79cd858
Prince Rathore <princeontheinternet@gmail.com>
2022-12-27 23:03:40 +0530

    updated

---
+++ 01_Python/Ch08_Functions/s1_functions_intro/p01_docstring_&_pep8.py
@@ -0,0 +1,23 @@
+def square(number: int) -> int :
+
+    """
+    A function that takes a number and returns the square of that number
+    """
+
+    return number**2
+
+print(square.__doc__)
+print(square(5))
+# help(square)
+
+
+# PEP 8 
+
+"""
+It is a document that provides guidelines and best practices on how to write Python code.
+PEP stands for Python Enhancement Proposal
+"""
+
+import this # The Zen Of Python
+
+# The above import statement is an easter egg that returns a poem type best practices of Python.
