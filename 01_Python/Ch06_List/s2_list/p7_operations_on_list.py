c6c3075dfefd933909ef5eb76e132a8dfc0e1e03
princeontheinternet <princeontheinternet@gmail.com>
2023-01-27 23:07:09 +0530

    updated

--- 01_Python/Ch06_List/s2_list/p7_operations_on_list.py
+++ 01_Python/Ch06_List/s2_list/p7_operations_on_list.py
@@ -7,34 +7,42 @@ list2 = [2, 4, 6, 8]
 list1.append(11)
 print(list1)  # [1, 3, 7, 9, 11]
 
-# 2. remove(takes exactly one argument) the item with the specified value
-list1.remove(11)
-print(list1)  # [1, 3, 7, 9]
-
-# 3. insert(takes exactly 2 arguments: index, object) adds a value to a specified index
+# 2. insert(takes exactly 2 arguments: index, object) adds a value to a specified index
 list2.insert(0, 0)
 print(list2)  # [0, 2, 4, 6, 8]
 
-# 4. count() of a specific item in a list
-print(list1.count(3))  # 1
-
-# 5. index(slice/index) of a first occurrence of specific item
-print(list1.index(3))  # 1
-
-# 6. sorts the list in place
-print(list1.sort())
-
-# 7. clear() removes all the elements
-# list1.clear()
-print(list1)  # []
-
-# 8. pop() removes the element at the specified position.
-# Default argument value is -1 i.e. the last element.
-# pop(4)
-
-# 9. extend() adds the list elements (or any iterable) to the end of the current list.
+# 3. extend() adds the list elements (or any iterable) to the end of the current list.
 list1.extend(list2)
 print(list1)        # [1, 3, 7, 9, 0, 2, 4, 6, 8]
+
+
+# 1. remove(takes exactly one argument) the item with the specified value
+list1.remove(11)
+print(list1)  # [1, 3, 7, 9]
+
+# 2. pop() removes the element at the specified index.
+# Default argument value is -1 i.e. the last element.
+list1.pop(-1)
+
+# 3. clear() removes all the elements
+list1.clear()
+print(list1)  # []
+
+# 4. del
+del list1[0]
+del list1   # deletes the entire variable in memory address
+
+
+# 1. count() of a specific item in a list
+print(list1.count(3))  # 1
+
+# 2. index(slice/index) of a first occurrence of specific item
+print(list1.index(3))  # 1
+
+# 3. sorts the list in place
+print(list1.sort())
+
+# ---------------------------------------------------------------------------------------------
 
 # 2. FUNCTIONS
 
@@ -43,3 +51,9 @@ print(len(list2))  # 5
 
 # 2. Min value in the list
 print(min(list2))  # 0
+
+# 3. Max value in the list
+print(max(list1))
+
+# 4. Sum of all elements
+print(sum(list1))
