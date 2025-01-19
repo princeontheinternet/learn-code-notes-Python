ccc7d13bba59e10552430cd22eaf1e9241d094f7
princeontheinternet <princeontheinternet@gmail.com>
2025-01-19 16:54:37 +0530

    Correct bmi code conditions to cover all ages 18.5, 25, and 30

--- 01_Python/Ch19_PythonQuestionnaire/Solutions/009_calculate_bmi/calculate_bmi.py
+++ 01_Python/Ch19_PythonQuestionnaire/Solutions/009_calculate_bmi/calculate_bmi.py
@@ -6,11 +6,11 @@ bmi = float(user_weight)/float(user_height)**2
 
 if bmi < 18.5:              
     print(f"Your BMI is {round(bmi)},  you are underweight")
-elif 18.5 < bmi < 25:   # chain comparison 
+elif 18.5 <= bmi < 25:   # chain comparison 
     print(f"Your BMI is {round(bmi)} you have a normal weight.") 
-elif 25 < bmi < 30:
+elif 25 <= bmi < 30:
     print(f"Your BMI is {round(bmi)}, you are slightly overweight.")
-elif 30 < bmi < 35:
+elif 30 <= bmi < 35:
     print(f"Your BMI is {round(bmi)}, you are obese.")
 elif bmi > 35:
     print(f"Your BMI is {round(bmi)}, you are clinically obese.")
